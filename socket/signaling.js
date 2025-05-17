5f47ca10cc4a009d87e9ad3945de4fa0d29a142b
ozXoz <honourkorkmaz@gmail.com>
2025-05-17 08:01:53 -0400

    signaling has been update it

--- socket/signaling.js
+++ socket/signaling.js
@@ -1,3 +1,25 @@
+// module.exports = (socket, io) => {
+//   console.log(`📶 [SIGNAL] ${socket.id} connected`);
+
+//   socket.on('signal', ({ to, data }) => {
+//     io.to(to).emit('signal', { from: socket.id, data });
+//   });
+
+//   socket.on('ready', roomId => {
+//     socket.join(roomId);
+//     socket.to(roomId).emit('peer-ready', socket.id);      // tell others
+
+//     // tell the newcomer who’s already there
+//     const peers = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+//     peers.forEach(id => id !== socket.id && socket.emit('peer-ready', id));
+//   });
+
+//   socket.on('disconnect', () =>
+//     io.emit('peer-disconnect', socket.id)
+//   );
+// };
+
+
 module.exports = (socket, io) => {
   console.log(`📶 [SIGNAL] ${socket.id} connected`);
 
@@ -5,16 +27,19 @@ module.exports = (socket, io) => {
     io.to(to).emit('signal', { from: socket.id, data });
   });
 
-  socket.on('ready', roomId => {
+  socket.on('join-room', roomId => {
     socket.join(roomId);
-    socket.to(roomId).emit('peer-ready', socket.id);      // tell others
+    console.log(`📡 ${socket.id} signaling joined ${roomId}`);
 
-    // tell the newcomer who’s already there
     const peers = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-    peers.forEach(id => id !== socket.id && socket.emit('peer-ready', id));
+    peers.forEach(id => {
+      if (id !== socket.id) {
+        socket.emit('user-joined', { id });
+      }
+    });
   });
 
-  socket.on('disconnect', () =>
-    io.emit('peer-disconnect', socket.id)
-  );
+  socket.on('disconnect', () => {
+    io.emit('peer-disconnect', socket.id);
+  });
 };
