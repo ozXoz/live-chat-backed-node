3e3dd1ea0f980558c650c63f1eb9fa402aaef8e5
ozXoz <honourkorkmaz@gmail.com>
2025-05-18 07:05:50 -0400

    update backend

--- utils/twilioIce.js
+++
@@ -1,7 +0,0 @@
-// server/utils/twilioIce.js
-import twilio from 'twilio';
-export async function fetchTwilioIce() {
-  const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
-  const token  = await client.tokens.create();      // POST /Tokens.json
-  return token.iceServers;                          // array
-}
